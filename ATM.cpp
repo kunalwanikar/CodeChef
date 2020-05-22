b1085e3f8c6053330c572613a6d743a2177b9409
Kunal Wanikar <kwanikar7@gmail.com>
2020-05-22 20:29:56 +0530

    Done with current version

--- ATM.cpp
+++ ATM.cpp
@@ -21,3 +21,19 @@ int main() {
 	}
 	return 0;
 }
+
+
+
+//INPUT
+//Positive integer 0 < X <= 2000 - the amount of cash which Pooja wishes to withdraw.
+//Nonnegative number 0<= Y <= 2000 with two digits of precision - Pooja's initial account balance.
+
+//OUTPUT
+//Output the account balance after the attempted transaction, given as a number with two digits of precision. 
+//If there is not enough money in the account to complete the transaction, output the current bank balance.
+
+//EXAMPLE
+//Input:
+//30 120.00
+//Output:
+//89.50
