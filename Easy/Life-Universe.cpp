38eadacc428da8116f1d825a09021e5f21d7691c
Kunal Wanikar <kwanikar7@gmail.com>
2020-05-23 01:01:39 +0530

    Create Life-Universe.cpp

---
+++ Easy/Life-Universe.cpp
@@ -0,0 +1,21 @@
+//https://www.codechef.com/problems/TEST
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+    while(true){
+        int n;
+        cin>>n;
+        if(n!=42){
+            cout<<n<<endl;
+        }
+        else{
+            break;
+        }
+    }
+    return 0;
+}
